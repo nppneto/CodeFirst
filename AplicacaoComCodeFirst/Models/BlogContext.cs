4415b7e36f663d560af9f12e39d3d394375b0e7c
Nelson <hawk5@DESKTOP-PF66N0S>
2017-10-05 22:22:00 -0300

    Estudo CodeFirst.

---
+++ AplicacaoComCodeFirst/Models/BlogContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AplicacaoComCodeFirst.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(): base("name=BlogContext")
+        {
+            Database.Connection.ConnectionString = 
+                @"Server=DESKTOP-PF66N0S\SQLEXPRESS;
+            Database=BlogBDLivro;
+            User ID=sa;Password=codinome";
+        }
+
+        public DbSet<Posts> Posts { get; set; }
+
+        public DbSet<Categorias> Categorias { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
